c0c79b355367ef3d8a7f16f6d4b7a11a68de73c3
NecroticGlow <157957530+NecroticGlow@users.noreply.github.com>
2024-12-20 19:57:36 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -38,7 +38,7 @@ if (screen.width >= 1) {
 			// cdnPath: "https://live2d-api.vercel.app/"
 			//apiPath: "https://api3.fghrsh.net/live2d/"
 			// cdnPath: "https://gitee.com/ajream/live2d_api/raw/master/"
-			cdnPath: "https://ghp.ci/https://raw.githubusercontent.com/NecroticGlow/live2d_api/master/"
+			cdnPath: "https://ghproxy.com/https://raw.githubusercontent.com/NecroticGlow/live2d_api/master/"
 		});
 	});
 }
